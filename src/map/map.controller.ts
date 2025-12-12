c676f04f519ab60a4b6117bdaf70fc2e10cd9085
SlowIntern <akshay22.henceforth@gmail.com>
2025-12-12 17:54:32 +0530

    distance

--- src/map/map.controller.ts
+++ src/map/map.controller.ts
@@ -1,7 +1,14 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post } from '@nestjs/common';
 import { MapService } from './map.service';
 
 @Controller('map')
 export class MapController {
-  constructor(private readonly mapService: MapService) {}
+  constructor(private readonly mapService: MapService) { }
+  
+
+  @Post('distance')
+  async getDistanceTime(pickup: string, destination: string) {
+    
+    return await this.mapService.getDistanceTime(pickup, destination);
+  }
 }
