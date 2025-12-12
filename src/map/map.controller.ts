41644c0c5c2d77cc91ec605efb64bc5dbcfeef60
SlowIntern <akshay22.henceforth@gmail.com>
2025-12-12 18:05:19 +0530

    the change skip

--- src/map/map.controller.ts
+++ src/map/map.controller.ts
@@ -1,14 +1,7 @@
-import { Controller, Post } from '@nestjs/common';
+import { Controller } from '@nestjs/common';
 import { MapService } from './map.service';
 
 @Controller('map')
 export class MapController {
-  constructor(private readonly mapService: MapService) { }
-  
-
-  @Post('distance')
-  async getDistanceTime(pickup: string, destination: string) {
-    
-    return await this.mapService.getDistanceTime(pickup, destination);
-  }
+  constructor(private readonly mapService: MapService) {}
 }
